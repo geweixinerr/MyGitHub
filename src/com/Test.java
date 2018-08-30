7fa235efc0ab8900bf455d6e7f1ac6241384def6
geweixinerr <geweixinerr@vip.163.com>
2018-08-30 21:32:34 +0800

    新增类

---
+++ src/com/Test.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("My GitHub!");
+	}
+}
