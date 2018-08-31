ba504c0f117ecb91715977c8c8aebd3527b6c371
geweixin <geweixinerr@vip.163.com>
2018-08-31 14:13:28 +0800

    删除冗余
    删除冗余代码

--- src/com/Test.java
+++ src/com/Test.java
@@ -8,8 +8,5 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("My GitHub!");
-		System.out.println("Hello World!");
-		System.out.println("Java");
-		System.out.println("Java Name");
 	}
 }
