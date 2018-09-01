baaefeec3736da60b59a018ff6d5e15d400216d4
葛伟新 <geweixinerr@vip.163.com>
2018-09-01 14:40:01 +0800

    comit

---
+++ src/com/Test.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("My GitHub is Very Good!");
+		System.out.println("Java is Very Good!");
+		System.out.println("Test2");
+	}
+}
