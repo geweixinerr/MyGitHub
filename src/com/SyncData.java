a3a17a7a476288bd9b1e54dd891e4983b3c0e6be
葛伟新 <geweixinerr@vip.163.com>
2018-09-02 12:32:36 +0800

    新增类
    如题，新增类，修订方法

---
+++ src/com/SyncData.java
@@ -0,0 +1,12 @@
+package com;
+
+public class SyncData {
+
+	public SyncData() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void Sync() {
+		System.out.println("我是Sync!");
+	}
+}
