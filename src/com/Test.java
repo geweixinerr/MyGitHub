36bd9898026220595b4fbf09762b153211653f14
geweixin <geweixinerr@vip.163.com>
2018-09-05 10:17:28 +0800

    VERY GOOD!

--- src/com/Test.java
+++ src/com/Test.java
@@ -9,6 +9,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("My GitHub is Very Good!");
 		System.out.println("Java is Very Good! OK!");
-		System.out.println("我是Git SmartGit!");
+		System.out.println("我是Git SmartGit!OK!");
 	}
 }
