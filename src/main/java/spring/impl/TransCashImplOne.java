5027ccd31f9e09123d7a4113a22cfd1c3f80232c
葛伟新 <geweixinerr@vip.163.com>
2018-10-04 10:53:49 +0800

    类文件修订
    如题
    
    Signed-off-by: 葛伟新 <geweixinerr@vip.163.com>

--- src/main/java/spring/impl/TransCashImplOne.java
+++ src/main/java/spring/impl/TransCashImplOne.java
@@ -2,6 +2,8 @@ package spring.impl;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import spring.intf.TransCashIntf;
@@ -9,13 +11,16 @@ import spring.intf.TransCashIntf;
 @Service("impl1")
 public class TransCashImplOne implements TransCashIntf{
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(TransCashImplOne.class);
+	
 	public TransCashImplOne() {
 
 	}
 
 	@Override
 	public BigDecimal moveMoney(BigDecimal money) {
-		System.out.println("第一个实现类!");
+		LOGGER.info("执行第一个实现类!");
+        System.out.println("第一个实现类!");
 		return null;
 	}
 }
--- src/main/java/spring/impl/TransCashImplTwo.java
+++ src/main/java/spring/impl/TransCashImplTwo.java
@@ -2,6 +2,8 @@ package spring.impl;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import spring.intf.TransCashIntf;
@@ -9,12 +11,16 @@ import spring.intf.TransCashIntf;
 @Service("impl2")
 public class TransCashImplTwo implements TransCashIntf{
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(TransCashImplTwo.class);
+
 	public TransCashImplTwo() {
 
 	}
 
 	@Override
 	public BigDecimal moveMoney(BigDecimal money) {
+		LOGGER.info("执行第二个实现类!");
+		LOGGER.debug("执行第二个实现类!");
 		System.out.println("第二个实现类!");
 		return null;
 	}
