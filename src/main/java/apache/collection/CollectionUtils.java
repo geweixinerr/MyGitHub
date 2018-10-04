3e237578ac7309764146a0eb04bf947ac3061545
葛伟新 <geweixinerr@vip.163.com>
2018-10-04 20:05:33 +0800

    新增pom依赖
    如题,新增joda日期依赖

--- src/main/java/apache/collection/CollectionUtils.java
+++ src/main/java/apache/collection/CollectionUtils.java
@@ -1,5 +1,8 @@
 package apache.collection;
 
+import java.util.Map;
+
+import org.apache.commons.collections4.map.LRUMap;
 import org.apache.commons.collections4.map.MultiKeyMap;
 
 /**
@@ -12,7 +15,7 @@ public final class CollectionUtils {
 	}
 
 	public static void main(String[] args) {
-		CollectionUtils.executeMultikeyMap();
+		CollectionUtils.executeLRUMap();
 	}
 	
 	/**
@@ -24,4 +27,19 @@ public final class CollectionUtils {
 	    String value = map.get("葛伟新", "江苏镇江");
 	    System.out.println("多Key-Map:" + value);	    
 	}
+	
+	/**
+	 * @author gewx LRU算法Map 
+	 * LRUMap，可以用来做缓存，它采用了LRU(least recently used)算法实现，当Map达到最大容量后，会优先删除掉最不经常被用到的项
+	 * **/
+	public static void executeLRUMap() {
+        Map<String,String> map = new LRUMap<String, String>(3); 
+        map.put("geweixin_01", "geweixin");
+        map.put("geweixin_02", "geweixin");
+        map.put("geweixin_03", "geweixin");
+        map.get("geweixin_01");
+        map.put("geweixin_04", "geweixin");
+        System.out.println("LRU-Map队列:" + map);
+	}
+	
 }
