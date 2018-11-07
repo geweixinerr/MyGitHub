59120ffd09dd7c207538b2bb6c3f2a0f5960b399
geweixin <geweixinerr@vip.163.com>
2018-11-07 15:56:02 +0800

    Netty目录结构变更

---
+++ src/main/java/netty/server/EchoServerHandler.java
@@ -0,0 +1,33 @@
+package netty.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * 示例Server Handler
+ **/
+public final class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		ByteBuf bf = (ByteBuf) msg;
+		bf.toString(CharsetUtil.UTF_8);
+		ctx.write(bf);
+	}
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+}
