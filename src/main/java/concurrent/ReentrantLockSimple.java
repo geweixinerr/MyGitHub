896981f456809e3ee1e66087ea3a1c1aedbf2e9c
geweixin <geweixinerr@vip.163.com>
2018-11-09 17:16:48 +0800

    新增ReentrantLock显示锁案例

---
+++ src/main/java/concurrent/ReentrantLockSimple.java
@@ -0,0 +1,116 @@
+package concurrent;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author gewx 显示锁案例
+ **/
+public final class ReentrantLockSimple {
+
+	public ReentrantLockSimple() {
+	}
+
+	private final ReentrantLock lock = new ReentrantLock();
+
+	public void syncOne() throws InterruptedException {
+		System.out.println("One进入锁区!");
+		try {
+			lock.lock();
+			TimeUnit.SECONDS.sleep(10);
+			System.out.println("SyncOne锁区执行!");
+		} finally {
+			lock.unlock();
+		}
+		System.out.println("One退出锁区!");
+	}
+
+	public void syncTwo() throws InterruptedException {
+		System.out.println("Two进入锁区!");
+		try {
+			lock.lock();
+			TimeUnit.SECONDS.sleep(10);
+			System.out.println("SyncTwo锁区执行!");
+		} finally {
+			lock.unlock();
+		}
+		System.out.println("Two退出锁区!");
+	}
+
+	public void syncOne_1() throws InterruptedException {
+		System.out.println("One进入锁区!");
+		if (lock.tryLock()) {
+			try {
+				TimeUnit.SECONDS.sleep(10);
+				System.out.println("SyncOne锁区执行!");
+			} finally {
+				lock.unlock();
+			}
+		}
+		System.out.println("One退出锁区!");
+	}
+
+	public void syncTwo_1() throws InterruptedException {
+		System.out.println("Two进入锁区!");
+		if (lock.tryLock()) {
+			try {
+				TimeUnit.SECONDS.sleep(10);
+				System.out.println("SyncTwo锁区执行!");
+			} finally {
+				lock.unlock();
+			}
+		}
+		System.out.println("Two退出锁区!");
+	}
+	
+	public void syncOne_2() throws InterruptedException {
+		System.out.println("One进入锁区!");
+		if (lock.tryLock()) {
+			try {
+				TimeUnit.SECONDS.sleep(10);
+				System.out.println("SyncOne锁区执行!");
+			} finally {
+				lock.unlock();
+			}
+		}
+		System.out.println("One退出锁区!");
+	}
+
+	public void syncTwo_2() throws InterruptedException {
+		System.out.println("Two进入锁区!");
+		if (lock.tryLock()) {
+			try {
+				TimeUnit.SECONDS.sleep(10);
+				System.out.println("SyncTwo锁区执行!");
+			} finally {
+				lock.unlock();
+			}
+		}
+		System.out.println("Two退出锁区!");
+	}
+	
+	public static void main(String[] args) {
+		final ReentrantLockSimple simple = new ReentrantLockSimple();
+		Executor executor = Executors.newFixedThreadPool(2);
+		executor.execute(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					simple.syncOne_1();
+				} catch (InterruptedException e) {
+				}
+			}
+		});
+		executor.execute(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					simple.syncTwo_1();
+				} catch (InterruptedException e) {
+				}
+			}
+		});
+	}
+}
