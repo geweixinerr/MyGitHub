9180c6302847e0fd798b1d34759d0cab1f0351d5
geweixinerr@vip.163.com <geweixinerr@vip.163.com>
2018-11-14 23:16:44 +0800

    Netty解决半包读写

--- src/main/java/netty/server/NettyEchoServer.java
+++ src/main/java/netty/server/NettyEchoServer.java
@@ -40,6 +40,8 @@ public final class NettyEchoServer {
 					.childHandler(new ChannelInitializer<SocketChannel>() {
 						@Override
 						public void initChannel(SocketChannel ch) throws Exception {
+							ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+							ch.pipeline().addLast(new StringDecoder());
 							ch.pipeline().addLast(echo);
 						}
 					});
