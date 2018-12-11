d02ac742dc5780205c34a1b473d873eb205dd4c0
geweixin <geweixinerr@vip.163.com>
2018-12-11 17:46:10 +0800

    注解代码修订

--- src/main/java/annotation/ListNotNull.java
+++ src/main/java/annotation/ListNotNull.java
@@ -9,21 +9,22 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 
 /**
-    ElementType.TYPE 用于类，接口，枚举但不能是注解
-    ElementType.FIELD 作用于字段，包含枚举值
-    ElementType.METHOD 作用于方法，不包含构造方法
-    ElementType.PARAMETER 作用于方法的参数
-    ElementType.CONSTRUCTOR 作用于构造方法
-    ElementType.LOCAL_VERIABLE 作用于本地变量或者catch语句
-    ElementType.ANNOTATION_TYPE 作用于注解
-    ElementType.PACKAGE 作用于包
-* **/
+ *ElementType.TYPE 用于类，接口，枚举但不能是注解
+ *ElementType.FIELD 作用于字段，包含枚举值
+ *ElementType.METHOD 作用于方法，不包含构造方法
+ *ElementType.PARAMETER 作用于方法的参数
+ *ElementType.CONSTRUCTOR 作用于构造方法
+ *ElementType.LOCAL_VERIABLE 作用于本地变量或者catch语句
+ *ElementType.ANNOTATION_TYPE 作用于注解
+ *ElementType.PACKAGE 作用于包
+ **/
 @Target({ElementType.FIELD})
+
 /**
-*  RetentionPolicy.SOURCE 注解存在于源代码中，编译时会被抛弃
-    RetentionPolicy.CLASS 注解会被编译到class文件中，但是JVM会忽略
-    RetentionPolicy.RUNTIME JVM会读取注解，同时会保存到class文件中[默认使用]
-* **/
+ *RetentionPolicy.SOURCE 注解存在于源代码中，编译时会被抛弃
+ *RetentionPolicy.CLASS 注解会被编译到class文件中，但是JVM会忽略
+ *RetentionPolicy.RUNTIME JVM会读取注解，同时会保存到class文件中[默认使用]
+ **/
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = {ListNotNullValid.class}) //指明验证类
 
