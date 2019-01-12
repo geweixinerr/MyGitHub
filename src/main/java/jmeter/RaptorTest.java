d14461c95f089be349d88cd4510e1e97fa14c10a
葛伟新 <geweixinerr@vip.163.com>
2019-01-12 20:13:49 +0800

    新增压测代码

--- src/main/java/jmeter/RaptorTest.java
+++ src/main/java/jmeter/RaptorTest.java
@@ -4,10 +4,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
 import org.apache.jmeter.samplers.SampleResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import raptor.RaptorRpc;
 import raptor.core.AbstractCallBack;
@@ -23,6 +29,8 @@ import raptor.core.message.RpcResponseBody;
  * Raptor压测
  **/
 public final class RaptorTest extends AbstractJavaSamplerClient {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(RaptorTest.class);
 
 	static {
 		System.out.println("初始化服务器参数...");
@@ -32,7 +40,7 @@ public final class RaptorTest extends AbstractJavaSamplerClient {
 		config.put("serverNode", "mc"); // 服务节点
 		config.put("remote", "localhost"); // 服务节点IP地址
 		config.put("port", "8090"); // 端口号
-		config.put("maxclients", "1024"); // 最大TCP连接数
+		config.put("maxclients", "8192"); // 最大TCP连接数
 		config.put("minclients", "6"); // 最小TCP连接数
 		config.put("speedNum", "1024"); // 最大单条TCP速率[在途事务数]
 
@@ -69,7 +77,7 @@ public final class RaptorTest extends AbstractJavaSamplerClient {
 
 				@Override
 				public void invoke(RpcRequestBody req, RpcResponseBody resp) {
-
+					
 				}
 			}, 5, data, message);
 			result.setSuccessful(true);
@@ -81,4 +89,43 @@ public final class RaptorTest extends AbstractJavaSamplerClient {
 		return result;
 	}
 
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		// 组装发送消息
+		String message = "Netty RPC Send, Netty is VeryGood!";
+		NettyTestData data = new NettyTestData();
+		
+		@SuppressWarnings("rawtypes")
+		RaptorRpc rpc = new RaptorRpc();
+		
+		Executor execute = Executors.newFixedThreadPool(10);
+		CyclicBarrier latch = new CyclicBarrier(10);
+
+		for (int i = 0; i < 10; i++) {
+			execute.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						latch.await();
+						// 发送异步消息.
+						for (int j = 0; j < 100000; j++) {
+							rpc.sendAsyncMessage("mc", "LoginAuth", new AbstractCallBack() {
+								@Override
+								public void invoke(RpcResponseBody resp) {
+								}
+								
+								@Override
+								public void invoke(RpcRequestBody req, RpcResponseBody resp) {
+									LOGGER.error("响应: " + req);
+								}
+								
+							}, 5, data, message);
+						}
+					} catch (InterruptedException | BrokenBarrierException e) {
+
+					}
+				}
+			});
+		}
+	}
 }
