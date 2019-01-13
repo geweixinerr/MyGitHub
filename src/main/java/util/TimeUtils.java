0bb50dcbda8fa0a4811615af917e817ca99510b7
葛伟新 <geweixinerr@vip.163.com>
2019-01-13 14:37:00 +0800

    TimeUnit测试类.

---
+++ src/main/java/util/TimeUtils.java
@@ -0,0 +1,17 @@
+package util;
+
+import java.util.concurrent.TimeUnit;
+
+public class TimeUtils {
+
+	public TimeUtils() {
+
+	}
+
+	public static void main(String[] args) {
+		long t1 = System.currentTimeMillis();
+		long t2 = TimeUnit.MICROSECONDS.convert(t1, TimeUnit.MILLISECONDS);
+		System.out.println("时间t1: " + t1);
+		System.out.println("时间t2: " + t2);
+	}
+}
