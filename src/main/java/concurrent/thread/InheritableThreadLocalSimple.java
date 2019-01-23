d0951bedefb81f49b9cc125029812c866c40c3a4
geweixin <geweixinerr@vip.163.com>
2019-01-23 20:03:21 +0800

    新增阿里threadlocal

--- src/main/java/concurrent/thread/InheritableThreadLocalSimple.java
+++ src/main/java/concurrent/thread/InheritableThreadLocalSimple.java
@@ -1,25 +1,32 @@
 package concurrent.thread;
 
+import com.alibaba.ttl.TransmittableThreadLocal;
+
 /**
  * @author gewx InheritableThreadLocal基本使用
  * **/
 public final class InheritableThreadLocalSimple {
 	
 	public static ThreadLocal<String> threadLocal = new InheritableThreadLocal<String>();
-	 
+	
+	public static TransmittableThreadLocal<String> threadLocal_2 = new TransmittableThreadLocal<String>();
+	
     public static void main(String args[]){
         threadLocal.set("Hello world.");
+        threadLocal_2.set("Hello world.");
  
         Thread thread = new MyThread();
         thread.start();
  
         System.out.println("main = " + threadLocal.get());
+        System.out.println("main = " + threadLocal_2.get());
     }
  
     static class MyThread extends Thread{
         @Override
         public void run(){
             System.out.println("MyThread = " + threadLocal.get());
+            System.out.println("MyThread = " + threadLocal_2.get());
         }
     }
 
