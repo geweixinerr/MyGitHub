6dae64e6f0098b58416f1a3fbe47fb1bd5266212
geweixin <geweixinerr@vip.163.com>
2019-01-24 17:17:52 +0800

    阿里巴巴ttl示例.

--- src/main/java/concurrent/alibaba/InheritableThreadLocalSimple.java
+++ src/main/java/concurrent/alibaba/InheritableThreadLocalSimple.java
@@ -1,32 +1,54 @@
-package concurrent.thread;
+package concurrent.alibaba;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import com.alibaba.ttl.TransmittableThreadLocal;
+import com.alibaba.ttl.threadpool.TtlExecutors;
 
 /**
  * @author gewx InheritableThreadLocal基本使用
  * **/
 public final class InheritableThreadLocalSimple {
 	
-	public static ThreadLocal<String> threadLocal = new InheritableThreadLocal<String>();
+	public static final ThreadLocal<String> threadLocal = new InheritableThreadLocal<String>();
 	
-	public static TransmittableThreadLocal<String> threadLocal_2 = new TransmittableThreadLocal<String>();
+	//alibaba开源组件
+	public static final TransmittableThreadLocal<String> t_threadLocal = new TransmittableThreadLocal<String>();
+		
+	//线程池
+	public static final Executor execute = TtlExecutors.getTtlExecutorService(Executors.newFixedThreadPool(1));
 	
     public static void main(String args[]){
         threadLocal.set("Hello world.");
-        threadLocal_2.set("Hello world.");
- 
-        Thread thread = new MyThread();
-        thread.start();
- 
-        System.out.println("main = " + threadLocal.get());
-        System.out.println("main = " + threadLocal_2.get());
+        t_threadLocal.set("Hello world.");
+        
+        execute.execute(new Runnable() {
+			@Override
+			public void run() {
+	            System.out.println("threadLocal = " + threadLocal.get());
+	            System.out.println("t_threadLocal = " + t_threadLocal.get());
+			}
+		});
+        
+        t_threadLocal.set("Hello world.Java");
+        
+        execute.execute(new Runnable() {
+			@Override
+			public void run() {
+	            System.out.println("threadLocal = " + threadLocal.get());
+	            System.out.println("t_threadLocal = " + t_threadLocal.get());
+			}
+		});
+        
     }
  
+    //Test thread
     static class MyThread extends Thread{
         @Override
         public void run(){
             System.out.println("MyThread = " + threadLocal.get());
-            System.out.println("MyThread = " + threadLocal_2.get());
+            System.out.println("MyThread = " + t_threadLocal.get());
         }
     }
 
