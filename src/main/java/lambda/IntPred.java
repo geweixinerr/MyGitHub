282d9424db68a77ca1a7dc10222e321175e93db3
geweixin <geweixinerr@vip.163.com>
2019-07-07 17:33:22 +0800

    新增案例.

---
+++ src/main/java/lambda/IntPred.java
@@ -0,0 +1,7 @@
+package lambda;
+
+@FunctionalInterface
+public interface IntPred {
+
+	boolean test(Integer value);
+}
---
+++ src/main/java/lambda/IntPredTest.java
@@ -0,0 +1,26 @@
+package lambda;
+
+import java.util.function.Predicate;
+
+public class IntPredTest {
+
+	public IntPredTest() {
+
+	}
+
+	public static boolean check(Predicate<Integer> predicate) {
+		return predicate.test(10);
+	}
+	
+	public static boolean check(IntPred predicate) {
+		return predicate.test(10);
+	}
+	
+	public static void main(String[] args) {
+		//重载正确作法
+		boolean b = IntPredTest.check((IntPred)x -> x.intValue() == 10);
+		
+		//boolean b = IntPredTest.check(x -> x.intValue() == 10); //Error
+		System.out.println("value: " + b);
+	}
+}
