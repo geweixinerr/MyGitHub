534ae440e3586e6a69ff11d1a42a3a21d787c1bd
geweixinerr@vip.163.com <geweixinerr@vip.163.com>
2019-09-01 22:25:19 +0800

    并发控制bugs fix.

--- src/main/java/concurrent/ConcurrentOneByOne.java
+++ src/main/java/concurrent/ConcurrentOneByOne.java
@@ -12,31 +12,37 @@ import exception.ConcurrentException;
 /**
  * @author gewx 并发处理,基于Redis setNx控制
  **/
-
 @Service
 public final class ConcurrentOneByOne {
 
 	private static final String VALUE = "true";
 
 	private static final int DEFAULT_TIME_OUT = 15;
-	
-	@Autowired
-	private RedisTemplate<String,String> redisTemplate;
 
-	public ConcurrentOneByOne addConcurrentKey(String _key) {		
-		this.key = _key;
-		this.timeOut = DEFAULT_TIME_OUT;
+	@Autowired
+	private RedisTemplate<String, String> redisTemplate;
+
+	private ThreadLocal<String> key = new ThreadLocal<>();
+
+	private ThreadLocal<Integer> timeOut = new ThreadLocal<>();
+
+	private ThreadLocal<String> tips = new ThreadLocal<>();
+
+	public ConcurrentOneByOne addConcurrentKey(String _key) {
+		key.set(_key);
+		timeOut.set(DEFAULT_TIME_OUT);
 		return this;
 	}
 
 	public ConcurrentOneByOne addKeyTimeOut(Integer _timeOut) {
-		this.timeOut = _timeOut;
+		timeOut.set(_timeOut);
 		return this;
 	}
 
-	private String key;
-
-	private Integer timeOut;
+	public ConcurrentOneByOne tips(String _tips) {
+		tips.set(_tips);
+		return this;
+	}
 
 	public <T> T execute(OneByOne<T> one) {
 		try {
@@ -47,40 +53,33 @@ public final class ConcurrentOneByOne {
 			after();
 		}
 	}
-	
+
 	private void before() {
-		if (StringUtils.isBlank(this.key)) {
+		String _key = key.get();
+		if (StringUtils.isBlank(_key)) {
 			throw new ConcurrentException("Concurrent Key is Not Empty~");
 		}
 
-		boolean isSet = redisTemplate.opsForValue().setIfAbsent(this.key, VALUE);
+		boolean isSet = redisTemplate.opsForValue().setIfAbsent(_key, VALUE);
 		if (!isSet) {
-			throw new ConcurrentException("并发业务逻辑处理中,请稍后再试~");
+			String _tips = tips.get();
+			if (StringUtils.isNotBlank(_tips)) {
+				throw new ConcurrentException(_tips);
+			} else {
+				throw new ConcurrentException("Concurrent Mode Fail~");
+			}
 		}
-		//此处命令存在连接中断的问题,即过期命令设置失败.
-		redisTemplate.expire(this.key,this.timeOut, TimeUnit.SECONDS);
+		// 此处命令存在连接中断的问题,即过期命令设置失败.
+		redisTemplate.expire(_key, timeOut.get(), TimeUnit.SECONDS);
 	}
-	
-	//需要高版本支持
-	/*
-	private void before() {
-		if (StringUtils.isBlank(this.key)) {
-			throw new ConcurrentException("Concurrent Key is Not Empty~");
-		}
-
-		boolean isSet = redisTemplate.opsForValue().setIfAbsent(this.key, VALUE, this.timeOut, TimeUnit.SECONDS);
-		if (!isSet) {
-			throw new ConcurrentException("并发业务逻辑处理中,请稍后再试~");
-		}
-	}
-	*/
 
 	private void after() {
 		try {
-			redisTemplate.delete(this.key);	
+			redisTemplate.delete(key.get());
 		} finally {
-			this.key = StringUtils.EMPTY;
-			this.timeOut = DEFAULT_TIME_OUT;	
-		}		
+			key.set(null);
+			timeOut.set(null);
+			tips.set(null);
+		}
 	}
 }
