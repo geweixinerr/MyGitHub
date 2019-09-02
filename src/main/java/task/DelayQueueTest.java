c23188b38a72fe392480e1555c4834fd80d25551
geweixin <geweixin@d2cmall.com>
2019-09-02 09:19:09 +0800

    案例fix.

--- src/main/java/task/DelayQueueTest.java
+++ src/main/java/task/DelayQueueTest.java
@@ -19,29 +19,37 @@ public class DelayQueueTest {
 	public static void main(String[] args) {
 		DateTime thisDate = new DateTime();
 		System.out.println("当前时间:  " + thisDate.toString(DATE_FORMAT_INSTANCE));
-		DateTime delayedDate = thisDate.plusSeconds(10);
+		DateTime delayedDate = thisDate.minusSeconds(10);
 		System.out.println("延迟时间:  " + delayedDate.toString(DATE_FORMAT_INSTANCE));
 
 		WxMsgPushTaskBeanDelayed test = new WxMsgPushTaskBeanDelayed();
+		test.setTaskId("1");
 		test.setTaskName("延迟任务队列~");
 		test.setExpire(delayedDate.toDate().getTime());
 		test.setTask(() -> {
 			System.out.println("延迟任务队列执行[1]~");
 		});
 
+		WxMsgPushTaskBeanDelayed _test = new WxMsgPushTaskBeanDelayed();
+		_test.setTaskId("1");
+		_test.setTaskName("延迟任务队列~");
+		_test.setExpire(delayedDate.toDate().getTime());
+		_test.setTask(() -> {
+			System.out.println("延迟任务队列执行[-1]~");
+		});
+		
 		WxMsgPushTaskBeanDelayed test2 = new WxMsgPushTaskBeanDelayed();
 		test2.setTaskName("延迟任务队列~");
 		test2.setExpire(thisDate.plusSeconds(20).toDate().getTime());
 		test2.setTask(() -> {
 			System.out.println("延迟任务队列执行[2]~");
 		});
-
-		delayQueue.add(test);
-		delayQueue.add(test);
+		
+		delayQueue.add(test);	
 		delayQueue.add(test2);
-
+		delayQueue.add(_test);
 		TaskBeanDelayed taskBean = null;
-		{
+		do {
 			taskBean = delayQueue.poll();
 			if (taskBean != null) {
 				System.out.println(taskBean.getTaskName());
@@ -49,6 +57,5 @@ public class DelayQueueTest {
 			}
 		}
 		while (taskBean != null);
-			
 	}
 }
