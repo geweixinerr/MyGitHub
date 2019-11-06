1e1028119d2b4e5de32f6e005dc00d80143a9ce0
geweixinerr@vip.163.com <geweixinerr@vip.163.com>
2019-11-06 17:50:16 +0800

    fix.

--- src/main/java/page/Page.java
+++ src/main/java/page/Page.java
@@ -13,11 +13,13 @@ public final class Page<T> implements Serializable {
 
 	private static final long serialVersionUID = 7108964245494609558L;
 
-	private int totalNum; //总行
-	
-	private int pageSize; //页行
-	
-	private int pageNum; //页码
-	
-	private List<T> page; //当前页数据集
+	private int totalNum; // 总行数
+
+	private int totalPageNum; // 总页数
+
+	private int pageSize; // 页行
+
+	private int pageNum; // 页码
+
+	private List<T> page; // 当前页数据集
 }
