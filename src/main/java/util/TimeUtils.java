9ea961ecee61247956023fdbab575ea09240ea3b
geweixin <geweixinerr@vip.163.com>
2019-12-24 15:16:10 +0800

    fix

--- src/main/java/util/TimeUtils.java
+++ src/main/java/util/TimeUtils.java
@@ -13,5 +13,6 @@ public class TimeUtils {
 		long t2 = TimeUnit.MICROSECONDS.convert(t1, TimeUnit.MILLISECONDS);
 		System.out.println("时间t1: " + t1);
 		System.out.println("时间t2: " + t2);
+		System.out.println("时间t3: " + t2);
 	}
 }
