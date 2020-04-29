71d81eada0515ed321bbbc5aec7a2eb28ab7f488
geweixinerr <geweixinerr@vip.163.com>
2020-04-29 19:03:45 +0800

    bugs fix

--- src/main/java/tree/Node.java
+++ src/main/java/tree/Node.java
@@ -1,11 +1,15 @@
 package tree;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
-import lombok.Getter;
-import lombok.Setter;
 
 /**
  * 树状结构顶层元素
@@ -43,4 +47,39 @@ public class Node implements Serializable {
 	@Getter
 	@Setter
 	protected List<Node> children = new ArrayList<>();
+
+	@Override
+	public final boolean equals(Object otherObject) {
+		if (otherObject == null) {
+			return false;
+		}
+
+		if (this == otherObject) {
+			return true;
+		}
+
+		Node otherNode = (Node) otherObject;
+		if (getClass() != otherNode.getClass()) {
+			return false;
+		}
+
+		EqualsBuilder builder = new EqualsBuilder();
+		builder.append(this.id, otherNode.id);
+		return builder.isEquals();
+	}
+
+	@Override
+	public final int hashCode() {
+		return this.id.hashCode() * 31;
+	}
+
+	@Override
+	public String toString() {
+		ToStringBuilder sb = new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE);
+		sb.append("id", this.id);
+		sb.append("parentId", this.parentId);
+		sb.append("sortNum", this.sortNum);
+		sb.append("children", this.children);
+		return sb.build();
+	}
 }
