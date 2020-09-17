c0ceb6827a401d3fc8cce39c9532df3bf94f6039
geweixin <geweixinerr@vip.163.com>
2020-09-17 23:03:55 +0800

    T1

--- src/main/java/util/TimeUtils.java
+++ src/main/java/util/TimeUtils.java
@@ -12,7 +12,6 @@ public class TimeUtils {
 		long t1 = System.currentTimeMillis();
 		long t2 = TimeUnit.MICROSECONDS.convert(t1, TimeUnit.MILLISECONDS);
 		System.out.println("时间t1: " + t1);
-		System.out.println("时间t2: " + t2);
 		
 		for (int i = 0; i < 100000; i++) {
 			println();
